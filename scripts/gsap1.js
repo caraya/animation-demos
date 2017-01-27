a09602b84391ac9b8b4fe5df4ada8a230c01fa85
Carlos Araya <carlos.araya@gmail.com>
2017-01-27 09:09:19 -0800

    latest version before publishing blog posts

--- scripts/gsap1.js
+++ scripts/gsap1.js
@@ -1,8 +1,1 @@
-let box1 = document.getElementById('box1');
 
-TweenLite.to("#box1", 4, { backgroundColor:"#ff00ff", width:"50%", height:"250px", ease:Power2.easeInOut });
-
-
-let box2 = document.getElementById('box2');
-
-TweenLite.fromTo("#box2", 1.5, {width:0, height:0}, {width:250, height:250});
